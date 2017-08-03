4cf2a5d38b00728dba3e2f3503fc2af7bf92ac58
Kent Allen <r.evolution801@gmail.com>
2017-08-03 23:27:01 +0000

    backup website from template, I am going to try starting from scratch

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-    res.send("Hello");
+    res.render("landing");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
