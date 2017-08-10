884b32d74a2dc9f7dfc88ed0fb1aa0a0a7a7262d
Kent Allen <r.evolution801@gmail.com>
2017-08-10 01:03:24 +0000

    Made significant progress working from scratch with most of the styling done. Still need to style About Me and add Portfolio

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,84 @@
-;(function(){
+var menuButtons = document.querySelectorAll(".menu-button");
+var menuIcon = document.querySelector(".menu-icon");
+var menuElement = document.querySelector(".nav");
+var menuDiv = document.querySelector(".menu-div");
+var mainDiv = document.querySelector(".main-div");
+var menuVisible = false;
+var iconColor;
 
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+console.log(menuButtons);
 
+for (var i = 0; i < menuButtons.length; i++) {
+    menuButtons[i].addEventListener("click", function() {
+        for (var i = 0; i < menuButtons.length; i++) {
+            menuButtons[i].classList.remove("active");
+        }
+        
+        this.classList.add("active");
+    });
+}
 
-})(jQuery)
+menuIcon.addEventListener("mouseenter", function() {
+    iconColor = window.getComputedStyle(menuIcon, null).getPropertyValue("color");
+    this.style.color = "red";
+});
+
+menuIcon.addEventListener("mouseleave", function() {
+    this.style.color = iconColor;
+});
+
+menuIcon.addEventListener("click", function() {
+    if(menuVisible) {
+        mainDiv.classList.add("col-lg-11");
+        mainDiv.classList.remove("col-lg-10");
+        
+        menuDiv.classList.add("col-lg-1");
+        menuDiv.classList.remove("col-lg-2");
+        
+        mainDiv.classList.add("col-xs-11");
+        mainDiv.classList.remove("col-xs-10");
+        
+        menuDiv.classList.add("col-xs-1");
+        menuDiv.classList.remove("col-xs-2");
+        menuElement.style.visibility = "hidden";
+        
+        menuIcon.classList.add("glyphicon-menu-hamburger");
+        menuIcon.classList.remove("glyphicon-menu-right");
+        menuIcon.style.color = "black";
+        iconColor = "black";
+        
+        menuDiv.style.background = "white";
+        
+        menuVisible = false;
+    } else {
+        mainDiv.classList.remove("col-lg-11");
+        mainDiv.classList.add("col-lg-10");
+        
+        menuDiv.classList.remove("col-lg-1");
+        menuDiv.classList.add("col-lg-2");
+        
+        mainDiv.classList.remove("col-xs-11");
+        mainDiv.classList.add("col-xs-10");
+        
+        menuDiv.classList.remove("col-xs-1");
+        menuDiv.classList.add("col-xs-2");
+        menuElement.style.visibility = "visible";
+        
+        menuIcon.classList.remove("glyphicon-menu-hamburger");
+        menuIcon.classList.add("glyphicon-menu-right");
+        menuIcon.style.color = "white";
+        iconColor = "white";
+        
+        menuDiv.style.background = "black";
+        
+        menuVisible = true;
+    }
+});
+
+// $(window).scroll(function(){
+//   $(".menu-div").stop().animate({"marginTop": ($(window).scrollTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "slow");
+// });
+
+$(window).scroll(function(){
+  $(".menu-div").css({"margin-top": ($(window).scrollTop()) + "px", "margin-left":($(window).scrollLeft()) + "px"});
+});
